5ce8513d68fc510e699bed126a07d566f4c37a48
Luiz-Althman <luizalthmandev@gmail.com>
2024-10-21 20:43:10 -0300

    publish

--- src/publish/publish.controller.ts
+++ src/publish/publish.controller.ts
@@ -1,32 +1,53 @@
-import { Controller, Get, Post, Body, Patch, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Delete,
+  UseGuards,
+} from '@nestjs/common';
 import { PublishService } from './publish.service';
 import { CreatePublishDto } from './dto/create-publish.dto';
 import { UpdatePublishDto } from './dto/update-publish.dto';
+import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { JwtAuthGuard } from '../auth/guard/jwt-auth.guard';
 
+@ApiTags('publish')
 @Controller('publish')
 export class PublishController {
   constructor(private readonly publishService: PublishService) {}
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @Post()
   create(@Body() createPublishDto: CreatePublishDto) {
     return this.publishService.create(createPublishDto);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @Get()
   findAll() {
     return this.publishService.findAll();
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @Get('by-id')
   findOne(@Body('id') id: number) {
     return this.publishService.findOne(+id);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @Get('user')
   findAllByUser(@Body('userId') userId: number) {
     return this.publishService.findAllByUser(userId);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @Patch()
   update(
     @Body()
@@ -40,6 +61,8 @@ export class PublishController {
     return this.publishService.update(id, userId, updatePublishDto);
   }
 
+  @ApiBearerAuth()
+  @UseGuards(JwtAuthGuard)
   @Delete()
   remove(@Body() body: { id: number; userId: number }) {
     const { id, userId } = body;
